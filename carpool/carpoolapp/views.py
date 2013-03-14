3e36598f208707a41ef86d6da8792e7876ec1d11
Peter Lee <nadapeter@gmail.com>
2013-03-13 22:21:56 -0700

    fixed some syntax errors

--- carpool/carpoolapp/views.py
+++ carpool/carpoolapp/views.py
@@ -65,41 +65,41 @@ def addroute(request):
     if (validDatums != 1):
     	resp = {"errCode" : validDatums}
     else:
-    	try:
-	        currentRoute = gmaps.directions(start, end)
-	        route = currentRoute['routes'][0]
-	        legs = route['legs']
+        try:
+            currentRoute = gmaps.directions(start, end)
+            route = currentRoute['routes'][0]
+            legs = route['legs']
 
-	        #dealing with possible multiple legs due to utilization of a waypoint
-	        for trip in legs:
-	            #print primRoute['routes'][end_location]
-	            #printing time and distance of route
-	            routeTime = trip['duration']['value'] / 60
-	            routeDist = trip['distance']['value'] * 0.000621371
-	            #formatting and printing each step
-	            directions[]
-	            count = 0
-	            #for adding turn by turn directions later
-	            for step in trip['steps']:
-	                indstep = step['html_instructions']
-	                indstep = indstep.replace('</b>', '')
-	                indstep = indstep.replace('<b>', '')
-	                indstep = indstep.replace('/<wbr/>', '')
-	                indstep = indstep.replace('<div style="font-size:0.9em">', ' *** ')
-	                indstep = indstep.replace('<div class="">', ' *** ')
-	                indstep = indstep.replace('<div class="google_note">', ' *** ')
-	                indstep = indstep.replace('</div>', ' *** ')
-	                directions[count] = indstep
-	                count++
+            #dealing with possible multiple legs due to utilization of a waypoint
+            for trip in legs:
+                #print primRoute['routes'][end_location]
+                #printing time and distance of route
+                routeTime = trip['duration']['value'] / 60
+                routeDist = trip['distance']['value'] * 0.000621371
+                #formatting and printing each step
+                directions=[]
+                count = 0
+                #for adding turn by turn directions later
+                for step in trip['steps']:
+                    indstep = step['html_instructions']
+                    indstep = indstep.replace('</b>', '')
+                    indstep = indstep.replace('<b>', '')
+                    indstep = indstep.replace('/<wbr/>', '')
+                    indstep = indstep.replace('<div style="font-size:0.9em">', ' *** ')
+                    indstep = indstep.replace('<div class="">', ' *** ')
+                    indstep = indstep.replace('<div class="google_note">', ' *** ')
+                    indstep = indstep.replace('</div>', ' *** ')
+                    directions[count] = indstep
+                    count += 1
 
-	        
-	        newRoute = Route(driver = uid, rider = "", depart_time = depart, maps_info = currentRoute, status = 0)
-	        newRoute.save()
+            
+            newRoute = Route(driver = uid, rider = "", depart_time = depart, maps_info = currentRoute, status = 0)
+            newRoute.save()
 
-	        resp = {"errCode" : 1}
+            resp = {"errCode" : 1}
 
-	    except Exception, err:
-	        resp = {"errCode" : err}
+        except Exception, err:
+            resp = {"errCode" : err}
     
     return HttpResponse(json.dumps(resp, cls=DjangoJSONEncoder), content_type = "application/json")
 
